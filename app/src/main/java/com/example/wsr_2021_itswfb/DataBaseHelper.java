bfae66b8cf4f9217d9c54b621b8009397041e69c
nikolajirtuganov <vongu3@gmail.com>
2021-02-27 22:32:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/wsr_2021_itswfb/DataBaseHelper.java
@@ -0,0 +1,93 @@
+package com.example.wsr_2021_itswfb;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    public static final String dbUSer = "UserDB";
+
+    public static final String userTable = "User";
+    public static final String colUserEmail = "Email";
+    public static final String colUserPass = "Password";
+
+    public DataBaseHelper(@Nullable Context context) {
+        super(context, dbUSer, null, 2);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + userTable + "(" +
+                colUserEmail + " TEXT PRIMARY KEY, " +
+                colUserPass + " TEXT NOT NULL);"
+                );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + userTable);
+        onCreate(db);
+    }
+
+    public Boolean RegUser(String email, String pass){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT COUNT(*) as 'count' FROM " + userTable +
+                " WHERE " + colUserEmail + "=?";
+        Cursor c = db.rawQuery(selectQuery, new String[]{email});
+        if(c!=null) {
+            c.moveToFirst();
+            Integer count = c.getInt(c.getColumnIndex("count"));
+            if (count == 0) {
+                db = this.getWritableDatabase();
+                ContentValues cv = new ContentValues();
+                cv.put(colUserEmail, email);
+                cv.put(colUserPass, pass);
+                db.insert(userTable, null, cv);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean AuthUser(String email, String pass){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT COUNT(*) as 'count' FROM " + userTable +
+                " WHERE " + colUserEmail + "=?";
+        Cursor c = db.rawQuery(selectQuery, new String[]{email});
+        if(c!=null){
+            c.moveToFirst();
+            Integer count = c.getInt(c.getColumnIndex("count"));
+            if (count == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<User> getUsers(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT " + colUserEmail + "," + colUserPass  + " FROM " + userTable;
+        Cursor c = db.rawQuery(selectQuery, null);
+        List<User> users = new ArrayList<>();
+        if(c!= null){
+            c.moveToFirst();
+            do {
+                String email = c.getString(c.getColumnIndex(colUserEmail));
+                String pass = c.getString(c.getColumnIndex(colUserPass));
+                User user = new User(email, pass);
+                users.add(user);
+            }while(c.moveToNext());
+        }
+        return users;
+    }
+}
---
+++ app/src/main/java/com/example/wsr_2021_itswfb/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.wsr_2021_itswfb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etEmail, etPass;
+    DataBaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPass = findViewById(R.id.etPass);
+
+        db = new DataBaseHelper(this);
+    }
+
+    public void btnSignInClick(View view) {
+        String email = etEmail.getText().toString();
+        String password = etPass.getText().toString();
+
+        Boolean success = db.AuthUser(email,password);
+        if(success){
+            Toast.makeText(this, "Вы вошли!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Ошибка входа!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void btnSignUpClick(View view) {
+        String email = etEmail.getText().toString();
+        String password = etPass.getText().toString();
+
+        Boolean success = db.RegUser(email, password);
+        if(success){
+            Toast.makeText(this, "Успешно зарегистрирован!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Такой пользователь уже существует!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
